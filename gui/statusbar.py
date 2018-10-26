0109dd06f66b6f364cd83d6557dd4aadadcc5208
Cristian García <cgarcia@datamatic.com.uy>
2018-10-26 09:44:24 -0300

    Pequeña optimización en el zoombar

--- gui/statusbar.py
+++ gui/statusbar.py
@@ -14,22 +14,29 @@ class DiscreteScale(Gtk.Scale):
         super().__init__(*args, **kwargs)
 
         values.sort()
-        self.values= values
+        self.values = values
+        self.__last_value = self.values[1]
 
-        adjustment= self.get_adjustment()
+        adjustment = self.get_adjustment()
         adjustment.set_lower(values[0])
         adjustment.set_upper(values[-1])
 
-        self.__changed_value_id = self.connect('change-value', self.__change_value)
+        self.__changed_value_id = self.connect("change-value", self.__change_value)
 
     def __change_value(self, scale, scroll_type, value):
-        # find the closest valid value
-        value= self.__closest_value(value)
-        # emit a new signal with the new value
+        value = self.__closest_value(value)
+        if value == self.__last_value:
+            return True
+
+        print(value)
+
         self.handler_block(self.__changed_value_id)
-        self.emit('change-value', scroll_type, value)
+        self.emit("change-value", scroll_type, value)
         self.handler_unblock(self.__changed_value_id)
-        return True #prevent the signal from escalating
+
+        self.__last_value = value
+
+        return True
 
     def __closest_value(self, value):
         return min(self.values, key=lambda v:abs(value-v))
