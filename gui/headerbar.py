2b71691d7015327700e4a1067e2a4c25cebd05df
Cristian García <cristian99garcia@gmail.com>
2018-11-03 15:22:04 -0300

    Arreglo error en la función gui -> headerbar.py -> HeaderBar -> _layout_size_changed en versiones antiguas de Gtk+

--- gui/headerbar.py
+++ gui/headerbar.py
@@ -18,6 +18,8 @@ class HeaderBar(Gtk.HeaderBar):
     def __init__(self):
         super(HeaderBar, self).__init__()
 
+        self._layout_size = list(CanvasConfig.DEFAULT_LAYOUT_SIZE)
+
         self.set_show_close_button(True)
         self._create_tool_size_buttons()
 
@@ -82,7 +84,7 @@ class HeaderBar(Gtk.HeaderBar):
         label = Gtk.Label.new("Ancho:")
         row.pack_start(label, False, False, 0)
 
-        default_width, default_height = CanvasConfig.DEFAULT_LAYOUT_SIZE
+        default_width, default_height = self._layout_size
 
         width_spinner = Gtk.SpinButton.new_with_range(1, 1024, 1)
         width_spinner.set_value(default_width)
@@ -98,14 +100,29 @@ class HeaderBar(Gtk.HeaderBar):
         height_spinner.set_value(default_height)
         row.pack_end(height_spinner, False, False, 0)
 
-        width_spinner.connect("value-changed", self._layout_size_changed, width_spinner, height_spinner)
-        height_spinner.connect("value-changed", self._layout_size_changed, width_spinner, height_spinner)
+        width_spinner.connect("value-changed", self._layout_size_changed, 0)
+        height_spinner.connect("value-changed", self._layout_size_changed, 1)
 
     def _tool_size_changed(self, button, size):
         self.emit("tool-size-changed", size)
 
-    def _layout_size_changed(self, widget, width_spinner, height_spinner):
-        size = (int(width_spinner.get_value()),
-                int(height_spinner.get_value()))
+    def _layout_size_changed(self, widget, index):
+        """
+        Esta función se modificó porque en versiones antiguas de Gtk al
+        parecer había un bug en el cual no se podía usar un objeto
+        heredado de Gtk.Widget como parámetro. En este caso, width_spinner
+        y height_spinner llegaban de la forma (notar el nil en la dirección
+        de memoria):
+        <Gtk.SpinButton object at 0x7fd2ed97bca8 (uninitialized at 0x(nil))>
 
-        self.emit("layout-size-changed", size)
+        Cuando deberían llegar de la forma:
+        <Gtk.SpinButton object at 0x7fd2ec39d750 (GtkSpinButton at 0x1b2a540)>
+
+        Por lo que al intentar llamar la función get_value en cualquiera
+        de estos dos parámtros se recibía:
+        (spryte.py:6715): Gtk-CRITICAL **: gtk_spin_button_get_value: assertion
+        'GTK_IS_SPIN_BUTTON (spin_button)' failed
+        """
+
+        self._layout_size[index] = int(widget.get_value())
+        self.emit("layout-size-changed", tuple(self._layout_size))
